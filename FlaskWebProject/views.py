d6cf4397bceef780922a1ef3efd1033e7afddd8f
messidagod <joeshakely@gmail.com>
2021-08-03 11:29:45 -0700

    added FlaskWebProject

---
+++ FlaskWebProject/views.py
@@ -0,0 +1,50 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from FlaskWebProject import app
+import os
+from flask import send_from_directory
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.jade',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.jade',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.jade',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+#rendering the HTML page which has the button
+@app.route('/json')
+def json():
+    return render_template('json.html')
+
+#background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    print ("Hello")
+    return ("nothing")
